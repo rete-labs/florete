805abb6a6125110f52739b850aad5571b62bcf10
Fyodor 'Wire Snark' Lyakhov <wsnark@rete-labs.ru>
2026-03-02 20:26:28 +0300

    feat: add high-level docs structure (#6)
    Closes #6

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { SiblingCards } from '@/components/sibling-cards';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    SiblingCards,
   };
 }
