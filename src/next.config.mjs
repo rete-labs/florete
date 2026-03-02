66728f3452e299861481539fafb3c312136ec28e
Fyodor 'Wire Snark' Lyakhov <wsnark@rete-labs.ru>
2026-03-02 12:09:42 +0300

    feat: serve GH Pages from florete.tech domain
    Closes #3

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -6,11 +6,6 @@ const withMDX = createMDX();
 const config = {
   output: 'export',
   reactStrictMode: true,
-  // Set Base Path for GitHub Pages
-  // If your repo is 'https://github.com/username/my-docs', set this to '/my-docs'
-  // If your repo is 'https://github.com/username/username.github.io', set this to ''
-  // FIXME: make this build-dependent: for local dev env it must be empty?
-  basePath: '/florete',
 
   // Required for Static Export with Next.js Images
   images: {
