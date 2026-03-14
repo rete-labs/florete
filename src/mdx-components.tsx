3aaa850d90d96af3dcb3b3605a5a8d2aada4bb63
Fyodor 'Wire Snark' Lyakhov <wsnark@rete-labs.ru>
2026-03-14 21:28:20 +0300

    feat: add HLD doc (#11)
    Closes #11

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { SiblingCards } from '@/components/sibling-cards';
+import { ImageBox } from '@/components/image-box';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
     SiblingCards,
+    ImageBox,
   };
 }
