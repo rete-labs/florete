df281f56044012d979b386c67fd592b7ed0f5385
Fyodor 'Wire Snark' Lyakhov <wsnark@rete-labs.ru>
2026-03-19 12:39:57 +0300

    feat: add design doc for C1. Manual Mesh milestone (#57)
    Closes #57

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,7 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { SiblingCards } from '@/components/sibling-cards';
 import { ImageBox } from '@/components/image-box';
+import { Mermaid } from '@/components/mermaid';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -9,5 +10,6 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...components,
     SiblingCards,
     ImageBox,
+    Mermaid
   };
 }
